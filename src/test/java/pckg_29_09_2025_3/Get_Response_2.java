8ff549f611c5c82ce8e944987eac8b645d841ae3
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-09-29 13:02:21 +0530

    Commit_GetRespone_1 & 2

---
+++ src/test/java/pckg_29_09_2025_3/Get_Response_2.java
@@ -0,0 +1,22 @@
+package pckg_29_09_2025_3;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Get_Response_2 {
+	
+	@Test
+	public void GetResponse() {
+		
+		Response response=RestAssured.get("https://api.restful-api.dev/objects?id=3&id=5&id=10");
+		
+		System.out.println("Response: " + response.asPrettyString());
+		
+		//Asset
+		
+		System.out.println("API RESPONSE CODE= " + response.getStatusCode() );
+	}
+
+}
