b4f4abe1d6ec5b56177b11519a2fa7cf788773af
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-01 23:45:16 +0530

    Test_3

--- src/test/java/Pckg_01_10_2025_2/GetResponseQuick.java
+++ src/test/java/Pckg_01_10_2025_2/GetResponseQuick.java
@@ -5,25 +5,24 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class Response_200_2 {
+public class GetResponseQuick {
 	
 	@Test
-	public void Response200() {
+	public void GetResponseQuick() {
 		
-		Response response= RestAssured.get("https://api.restful-api.dev/objects/7");
+		Response response= RestAssured.get("https://api.restful-api.dev/objects");
 		
 		System.out.println("Response : " + response.asPrettyString());
 		
 		//Assertion
 		
-		System.out.println("API RESPONSE CODE = " + response.getStatusCode() );
+		System.out.println("API RESPONSE CODE = " + response.statusCode());
 		
 		System.out.println("id are : " + response.jsonPath().get("id"));
 		
 		System.out.println("name value are : " + response.jsonPath().get("name"));
 		
-		System.out.println("data value are : " + response.jsonPath().get("data.price"));
-		
+		System.out.println("data value are : " + response.jsonPath().get("data.value"));
 	}
 
 }
