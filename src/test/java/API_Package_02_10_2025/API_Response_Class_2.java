af885dec07ddc47edff6560857acd112fb73fdef
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-02 07:23:26 +0530

    Test_Response_2

---
+++ src/test/java/API_Package_02_10_2025/API_Response_Class_2.java
@@ -0,0 +1,28 @@
+package API_Package_02_10_2025;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class API_Response_Class_2 {
+	
+	@Test
+	public void GetResponse() {
+		
+		Response response=RestAssured.get("https://api.restful-api.dev/objects?id=3&id=5&id=10");
+		
+		System.out.println("Response : " + response.asPrettyString());
+		
+		//Assertion
+		
+		System.out.println("API RESPONSE CODE = " + response.statusCode());
+		
+		System.out.println("id are : " + response.jsonPath().get("id"));
+		
+		System.out.println("name value are : " + response.jsonPath().get("name"));
+		
+		System.out.println("data value are : " + response.jsonPath().get("data.price"));
+	}
+
+}
