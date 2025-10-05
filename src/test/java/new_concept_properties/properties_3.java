4ebc8a9a0794b4cacdc7d8598211fd80909929cb
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-05 23:50:31 +0530

    File3

--- src/test/java/new_concept_properties/properties_3.java
+++ src/test/java/new_concept_properties/properties_3.java
@@ -1,9 +1,22 @@
 package new_concept_properties;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class properties_3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		//File path
+		
+		File f=new File("C:\\ChaitanyaAde\\eclipse_workspace\\API_Response_28_09_2025\\src\\test\\resources\\SYS.properties");
+		FileInputStream fis=new FileInputStream(f);
+		properties_3 prop=new properties_3();
+		//load files
+		properties_3.load()
+		
+		
 
 	}
 
