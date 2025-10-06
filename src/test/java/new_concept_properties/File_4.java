39cbec64ddc95bbb6ff535bad6cd4f5828078847
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-06 08:22:16 +0530

    Class_4

---
+++ src/test/java/new_concept_properties/File_4.java
@@ -0,0 +1,25 @@
+package new_concept_properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class File_4 {
+
+	public static void main(String[] args)  throws FileNotFoundException { 
+		// TODO Auto-generated method stub
+		
+		//File path
+		
+		File f=new File("C:\\ChaitanyaAde\\eclipse_workspace\\API_Response_28_09_2025\\src\\test\\resources\\SYS.properties");
+		
+		FileInputStream fis=new FileInputStream(f);
+		
+		File_4 f1=new File_4();
+		
+		//file load
+		Object prop;
+
+	}
+
+}
