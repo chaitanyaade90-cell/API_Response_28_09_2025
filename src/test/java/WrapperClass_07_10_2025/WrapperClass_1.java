2f26c7b23362d7d8d5ac412d02010c966205e982
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-07 14:45:42 +0530

    Wrapper Class-1

---
+++ src/test/java/WrapperClass_07_10_2025/WrapperClass_1.java
@@ -0,0 +1,28 @@
+package WrapperClass_07_10_2025;
+
+public class WrapperClass_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String S1 = "10";
+		String S2 = "20";
+		
+		System.out.println(Integer.parseInt(S1)+ Integer.parseInt(S2));
+		
+		
+		String S3= "12.5";
+		String S4= "13.2";
+		
+		System.out.println(Double.parseDouble(S3)+Double.parseDouble(S4));
+		
+		
+		String S5= "true";
+		System.out.println(Boolean.parseBoolean(S5));
+		
+		
+		
+
+	}
+
+}
