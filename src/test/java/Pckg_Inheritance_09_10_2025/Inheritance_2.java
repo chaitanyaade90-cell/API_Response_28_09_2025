5ea1c434729737a1127b17eae2cf472af7ac349c
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-09 14:20:43 +0530

    Response_4

--- src/test/java/Pckg_Inheritance_09_10_2025/Inheritance_2.java
+++ src/test/java/Pckg_Inheritance_09_10_2025/Inheritance_2.java
@@ -1,9 +1,53 @@
 package Pckg_Inheritance_09_10_2025;
 
+class I
+{
+	
+   int i=200;
+   
+   void inherit() {
+	   
+	   System.out.println(i);
+   }
+	   
+	   
+   }
+
+class J extends I
+
+{ 
+	
+	int j=300;
+	
+	void inherit2() {
+		
+		System.out.println(j);
+	}
+}
+	
+
+
+
+
 public class Inheritance_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		I obj=new I();
+		
+		obj.inherit();
+		
+		System.out.println("**********");
+		
+		
+		J obj2=new J();
+		
+		obj2.inherit2();
+		
+		
+		
+		
 
 	}
 
