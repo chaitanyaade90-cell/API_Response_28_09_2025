988de548f2451adc90d9fc8052ad52816e850525
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-09 12:49:09 +0530

    Response_4

---
+++ src/test/java/Pckg_Inheritance_09_10_2025/Inheritance_Class_1.java
@@ -0,0 +1,10 @@
+package Pckg_Inheritance_09_10_2025;
+
+public class Inheritance_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/pckg_09_10_2025/Response_200.java
@@ -0,0 +1,31 @@
+package pckg_09_10_2025;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Response_200 {
+	
+	@Test
+	public void Get200Response() {
+		
+		Response response=RestAssured.get("https://api.restful-api.dev/objects/7");
+		
+		System.out.println("Response : " + response.asPrettyString());
+		
+		//Assertion
+		
+		System.out.println("Response = " + response.statusCode());
+		
+		System.out.println("id are : " + response.jsonPath().get("id"));
+		
+		System.out.println("name value are : " + response.jsonPath().get("name"));
+		
+		System.out.println("data value are : " + response.jsonPath().get("data.price"));
+		
+	}
+	
+	
+
+}
