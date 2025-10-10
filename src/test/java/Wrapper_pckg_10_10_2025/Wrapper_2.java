e65c6a451b8aefd09d62dd24cbad9c25f076ec4f
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-10 12:48:43 +0530

    Wrapper_2

---
+++ src/test/java/Wrapper_pckg_10_10_2025/Wrapper_2.java
@@ -0,0 +1,26 @@
+package Wrapper_pckg_10_10_2025;
+
+public class Wrapper_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String S1= "2000";
+		String S2="3000";
+		
+		System.out.println(Integer.parseInt(S1)+ Integer.parseInt(S2));
+		
+		
+		String S3= "9000";
+		String S4= "7000";
+		
+		System.out.println();
+		
+		
+		String S5="true";
+		
+		System.out.println(Boolean.getBoolean(S5));
+
+	}
+
+}
