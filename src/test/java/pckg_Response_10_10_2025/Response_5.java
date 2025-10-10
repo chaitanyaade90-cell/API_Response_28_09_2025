1d26f7b1a7bb89a7db3e789af1f6c97563cd5bfc
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-10 22:27:07 +0530

    Response_5

---
+++ src/test/java/pckg_Response_10_10_2025/Response_5.java
@@ -0,0 +1,29 @@
+package pckg_Response_10_10_2025;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Response_5 {
+	
+	@Test
+	public void Get200() {
+		
+		
+		Response response=RestAssured.get("https://api.restful-api.dev/objects?id=3&id=5&id=10");
+		
+		System.out.println("Response : " + response.asPrettyString());
+		
+		//Assertion
+		
+		System.out.println("Response = " + response.statusCode());
+		
+		System.out.println("id are : " + response.jsonPath().get("id"));
+		
+		System.out.println("name value are : " + response.jsonPath().get("name"));
+		
+		System.out.println("data value are : " + response.jsonPath().get("data.price"));
+	}
+
+}
