1b8daa33590bf9d799680d218cfa47afc4fef1f9
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-10 11:31:23 +0530

    WrapperClass_1

---
+++ src/test/java/Wrapper_pckg_10_10_2025/Wrapper_Class_1.java
@@ -0,0 +1,29 @@
+package Wrapper_pckg_10_10_2025;
+
+public class Wrapper_Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String S1= "200";
+		String S2= "300";
+		
+		System.out.println(Integer.parseInt(S1)+Integer.parseInt(S2));
+		
+		
+		
+		
+		String S3="12.5";
+		String S4="13.5";
+		
+	
+		System.out.println(Double.parseDouble(S3)+Double.parseDouble(S4));
+		
+		
+		String S5="true";
+		
+		System.out.println(Boolean.parseBoolean(S5));
+
+	}
+
+}
