3da5a874cf6682334fe5f23557d24312639b7d5d
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-11 14:58:24 +0530

    Method_3_Test

---
+++ src/test/java/Method_Packg_11_10_2025/Method_3.java
@@ -0,0 +1,38 @@
+package Method_Packg_11_10_2025;
+
+public class Method_3 {
+	
+	int number;
+	String name;
+	int salary;
+	
+	void sum(int number1, String name1, int salary1)
+	{
+		
+		number=number1;
+		name=name1;
+		salary=salary1;
+		
+	}
+	
+	void display()
+	{
+		
+		System.out.println(number);
+		System.out.println(name);
+		System.out.println(salary);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Method_3 ME=new Method_3();
+		ME.sum(2000, "Rohit", 25000);
+		ME.display();
+		
+
+	}
+
+}
