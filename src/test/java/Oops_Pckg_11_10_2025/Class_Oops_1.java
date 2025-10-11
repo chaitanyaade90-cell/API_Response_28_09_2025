096fbdca6587a96626d1faa74df3bb4865c12693
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-11 10:04:16 +0530

    Ops_1

---
+++ src/test/java/Oops_Pckg_11_10_2025/Class_Oops_1.java
@@ -0,0 +1,29 @@
+package Oops_Pckg_11_10_2025;
+
+public class Class_Oops_1 {
+	
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void Display() {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Class_Oops_1 Op=new Class_Oops_1();
+		Op.id=90;
+		Op.name="chaitanya";
+		Op.salary=50000;
+		Op.Display();
+
+	}
+
+}
