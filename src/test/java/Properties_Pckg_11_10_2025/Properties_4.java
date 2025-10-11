0c90efad3e69229b83750a0fa2c45be54298038a
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-11 22:53:24 +0530

    prop_4

---
+++ src/test/java/Properties_Pckg_11_10_2025/Properties_4.java
@@ -0,0 +1,28 @@
+package Properties_Pckg_11_10_2025;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Properties_4 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+	   //File Path
+		
+		File f=new File("C:\\ChaitanyaAde\\eclipse_workspace\\API_Response_28_09_2025\\src\\test\\resources\\SYS.properties");
+        
+		FileInputStream fis= new FileInputStream(f);
+		
+		Properties prop=new Properties();
+		
+		// File Upload
+		
+		prop.load(fis);
+		
+		System.out.println(prop.get("endpoing1_api_url"));
+	}
+
+}
