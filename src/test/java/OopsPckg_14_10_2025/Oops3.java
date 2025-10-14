21260c1ad11a83ec8f2c9366d83ace321de2bd65
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-14 11:30:16 +0530

    TestOops_3

---
+++ src/test/java/OopsPckg_14_10_2025/Oops3.java
@@ -0,0 +1,32 @@
+package OopsPckg_14_10_2025;
+
+import Oops_pckg_09_10_2025.Oops_3;
+
+public class Oops3 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void display() {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Oops3 Op=new Oops3();
+		Op.id=23;
+		Op.name="Roclex";
+		Op.salary=20000;
+		Op.display();
+		
+		
+		
+
+	}
+
+}
