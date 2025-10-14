811e7cb2d71631c38c0938471a11ab9c900e1d46
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-14 12:55:45 +0530

    LoginFb_2

---
+++ src/test/java/Selenium_project_14_10_2025/LoginPage_2.java
@@ -0,0 +1,33 @@
+package Selenium_project_14_10_2025;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPage_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1=new ChromeDriver();
+		
+		d1.get("https://www.facebook.com/");
+		
+		d1.manage().window().maximize();
+		
+		WebElement emailid=d1.findElement(By.name("email"));
+		emailid.sendKeys("chaitanyaade90@gmail.com");
+		
+		WebElement password=d1.findElement(By.name("pass"));
+		password.sendKeys("09chaitanys");
+		
+		WebElement loginbutton=d1.findElement(By.name("login"));
+		loginbutton.click();
+
+	}
+
+}
