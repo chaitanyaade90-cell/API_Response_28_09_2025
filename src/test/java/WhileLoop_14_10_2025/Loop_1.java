c779fcc7bd7d8c2e4f400b5fab6764f855c96762
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-14 16:09:29 +0530

    WhileLoop3

---
+++ src/test/java/WhileLoop_14_10_2025/Loop_1.java
@@ -0,0 +1,18 @@
+package WhileLoop_14_10_2025;
+
+public class Loop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 3; i < 51; i++) {
+			
+			System.out.println(i);
+			
+			
+			
+		}
+
+	}
+
+}
---
+++ src/test/java/WhileLoop_14_10_2025/WhileLoop_2.java
@@ -0,0 +1,20 @@
+package WhileLoop_14_10_2025;
+
+public class WhileLoop_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1; // initialization
+		
+		while(i<=5) { // condition
+			System.out.println("Number : " + i);
+			
+			i++; // increment
+			
+		}
+		
+
+	}
+
+}
