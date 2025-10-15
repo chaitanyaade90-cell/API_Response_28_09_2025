e4b7aa694b495e5d1c18d17734cb3e8e8fd148dc
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-15 08:00:03 +0530

    WhileLoop1

---
+++ src/test/java/WhileLoopPackage_15_10_2025.java
@@ -0,0 +1,15 @@
+
+public class WhileLoopPackage_15_10_2025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 3; i < 101; i++) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
