88e0528f03deca78386e0d737c3184cfc7ba4be8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-20 18:31:49 +0530

    Selenium_1

---
+++ src/test/java/SeleniumProject_20_10_2025/Selenium_1.java
@@ -0,0 +1,33 @@
+package SeleniumProject_20_10_2025;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1=new ChromeDriver();
+		
+		d1.get("https://www.facebook.com/");
+		
+		WebElement emailid=d1.findElement(By.id("email"));
+		emailid.sendKeys("chaitanyaade90@gmail.com");
+		
+		WebElement password=d1.findElement(By.name("pass"));
+		password.sendKeys("09chaitanys");
+		
+		WebElement loginbutton=d1.findElement(By.name("login"));
+		loginbutton.click();
+		
+		
+
+	}
+
+}
