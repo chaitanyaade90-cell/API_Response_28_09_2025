8e609e3c04fb31776630c4baeba0856be54d8b88
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-10-22 22:19:31 +0530

    Screenshots_3

---
+++ src/test/java/ScreenShotsExamples_22_10_2025/Screenshots_3.java
@@ -0,0 +1,36 @@
+package ScreenShotsExamples_22_10_2025;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Screenshots_3 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1=new ChromeDriver();
+		
+		d1.get("https://www.google.com");
+		
+		d1.manage().window().maximize();
+		
+		File src=((TakesScreenshot)d1).getScreenshotAs(OutputType.FILE);
+		
+		File dest= new File("C:\\Screenshots\\google.png");
+		
+		FileUtils.copyFile(src, dest);
+		
+		System.out.println("Screenshots saved Succesfully");
+
+	}
+
+}
