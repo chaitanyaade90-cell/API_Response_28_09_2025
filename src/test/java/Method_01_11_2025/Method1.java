3e6cdf151b20d4400710fd7f0f6f425748ae43d3
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-01 15:40:01 +0530

    Method1

---
+++ src/test/java/Method_01_11_2025/Method1.java
@@ -0,0 +1,29 @@
+package Method_01_11_2025;
+
+public class Method1 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void sum(int id1, String string, int salary1) {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+	}
+	
+	void display() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Method1 m1=new Method1();
+		m1.sum(100, "Chaitanya", 20000);
+		m1.display();
+
+	}
+
+}
