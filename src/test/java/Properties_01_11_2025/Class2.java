ede48fddd586903d814461e37b605d640140b599
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-01 12:53:47 +0530

    prop2

---
+++ src/test/java/Properties_01_11_2025/Class2.java
@@ -0,0 +1,26 @@
+package Properties_01_11_2025;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Class2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		File f=new File("C:\\ChaitanyaAde\\eclipse_workspace\\API_Response_28_09_2025\\src\\test\\resources\\SYS.properties"); 
+        
+		FileInputStream fis=new FileInputStream(f);
+		
+		Properties prop=new Properties();
+		
+		prop.load(fis);
+		
+		System.out.println(prop.get("endpoing1_api_url"));
+		
+		
+	}
+
+}
