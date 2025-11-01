46a59b8613a043548e7a60f2f798cf4da7c41e47
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-01 18:12:53 +0530

    Loop3web

--- src/test/java/DoLoop_01_11_2025/Loopwebsites_3.java
+++ src/test/java/DoLoop_01_11_2025/Loopwebsites_3.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoopWebSites_1 {
+public class Loopwebsites_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[]websites = {
+		String[]websites= {
 				"https://www.google.com",
 	            "https://www.facebook.com",
 	            "https://www.amazon.com",
@@ -19,16 +19,15 @@ public class LoopWebSites_1 {
 		
 		WebDriverManager.chromedriver();
 		
-		ChromeDriver d1= new ChromeDriver();
+		ChromeDriver d1=new ChromeDriver();
 		
-		d1.manage().window().maximize();
 		for (int i = 0; i < websites.length; i++) {
+			
+			System.out.println("website : " + websites[i]);
+			System.out.println("Title :  " + d1.getTitle());
 			
 		}
 		
-		d1.get(websites[i]); // open websites
-		System.out.println("websites: " + websites[i]);
-		System.out.println("Title: " + d1.getTitle());
 
 	}
 
