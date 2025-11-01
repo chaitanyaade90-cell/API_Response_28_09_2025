d50b0b87ec19885f29c450e430137903ebb78e59
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-01 16:56:25 +0530

    Doloop1

---
+++ src/test/java/DoLoop_01_11_2025/DoLoop1.java
@@ -0,0 +1,21 @@
+package DoLoop_01_11_2025;
+
+public class DoLoop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =1;
+		
+		do {
+			
+			System.out.println("Hello " + i);
+			
+			i++;
+			
+			
+		}while (i<=3);
+
+	}
+
+}
---
+++ src/test/java/DoLoop_01_11_2025/package-info.java
@@ -0,0 +1,1 @@
+package DoLoop_01_11_2025;
