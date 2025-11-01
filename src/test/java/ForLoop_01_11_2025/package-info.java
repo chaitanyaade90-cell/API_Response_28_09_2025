1eb59094c31ff90d600f96f9a4fc5af80fd4a25e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-01 16:09:35 +0530

    forLop1

---
+++ src/test/java/ForLoop_01_11_2025/package-info.java
@@ -0,0 +1,1 @@
+package ForLoop_01_11_2025;
---
+++ src/test/java/Method_01_11_2025/Method2.java
@@ -0,0 +1,35 @@
+package Method_01_11_2025;
+
+public class Method2 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void sum(int id1, int String, int salary1) {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+		
+	}
+	
+	void display() {
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Method2 mt=new Method2();
+		mt.id=22;
+		mt.name="Coffee";
+		mt.salary=20000;
+		mt.display();
+		
+		
+
+	}
+
+}
