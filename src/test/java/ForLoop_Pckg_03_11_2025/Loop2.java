34e8f73a1c0728d6a9b8c40a748ff420686b0565
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-03 20:20:53 +0530

    Api_4

---
+++ src/test/java/ForLoop_Pckg_03_11_2025/Loop2.java
@@ -0,0 +1,17 @@
+package ForLoop_Pckg_03_11_2025;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 5; i < 100; i++) {
+			
+			System.out.println(i);
+			
+			
+		}
+
+	}
+
+}
