1b4ae99a9d83c0b7ccaee9271e9ac1c0261f2a88
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-04 17:46:48 +0530

    List2

--- src/test/java/ListPckg_04_11_2025/ListNames2.java
+++ src/test/java/ListPckg_04_11_2025/ListNames2.java
@@ -3,27 +3,30 @@ package ListPckg_04_11_2025;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NamesList1 {
+public class ListNames2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		List<String> names= new ArrayList<>();
+		List<String> names=new ArrayList();
 		
-		names.add("Steve");
-		names.add("Slaver");
-		names.add("Steal");
-		names.add("Jones");
+		names.add("Class1");
+		names.add("Class2");
+		names.add("Class3");
+		names.add("Stad1");
 		
-		System.out.println("Names starting with 'S' : ");
+		System.out.println("Names starting with 'C' : ");
 		
 		for (int i = 0; i < names.size(); i++) {
 			
 			String name=names.get(i);
 			
-			if(name.startsWith("S")) {
+			if(name.startsWith("C")) {
+				
 				System.out.println(name);
 			}
+			
+			
 			
 		}
 
