078a08f9c569b37706154a6d747a69d3e92d7e97
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-05 23:12:23 +0530

    DoIfExample_1

---
+++ src/test/java/DoLoopPackage_05_11_2025/DoWhileFalseExample_3.java
@@ -0,0 +1,20 @@
+package DoLoopPackage_05_11_2025;
+
+public class DoWhileFalseExample_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=0;
+		
+		do {
+			System.out.println("This will run once even if condition is false");
+		
+		}while (x < 5);
+		
+		
+		
+
+	}
+
+}
