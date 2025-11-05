3725b071301b60afe2224404690a091ef153aa10
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-05 23:06:03 +0530

    DoLoop2

---
+++ src/test/java/DoLoopPackage_05_11_2025/Do_Loop_2.java
@@ -0,0 +1,24 @@
+package DoLoopPackage_05_11_2025;
+
+public class Do_Loop_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1;
+		
+		int sum=0;
+		
+		do {
+			
+			sum=sum +i;
+			
+			i++;
+			
+		} while (i <=5);
+		
+		System.out.println("Total sum=" + sum);
+
+	}
+
+}
