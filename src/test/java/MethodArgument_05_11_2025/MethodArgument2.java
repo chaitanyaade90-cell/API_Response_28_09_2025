caefa6e5de2c396cd92b9608f0dd8197262317b2
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-05 08:46:10 +0530

    MethodArgument2

---
+++ src/test/java/MethodArgument_05_11_2025/MethodArgument2.java
@@ -0,0 +1,28 @@
+package MethodArgument_05_11_2025;
+
+public class MethodArgument2 {
+	
+	public static void Add(int a1, int a2) {
+		int sum=a1+a2;
+		System.out.println(sum);
+	}
+	public static void sub(int b1, int b2) {
+		int sum=b1-b2;
+		System.out.println(sum);
+		
+	}
+	public static void mul(int c1, int c2) {
+		int sum=c1*c2;
+		System.out.println(sum);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodArgument2.Add(100, 200);
+		MethodArgument2.sub(200, 100);
+		MethodArgument2.mul(500, 200);
+
+	}
+
+}
