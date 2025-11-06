34b87b84a67248fd3fbcf37534e506e9db8cbf56
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-06 09:16:02 +0530

    Oops2

---
+++ src/test/java/Oops_package_06_11_2025/Oops2.java
@@ -0,0 +1,29 @@
+package Oops_package_06_11_2025;
+
+public class Oops2 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void display() {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Oops2 op=new Oops2();
+		op.id=30;
+		op.name="Caves";
+		op.salary=89990;
+		op.display();
+
+	}
+
+}
