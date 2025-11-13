8742fbb80f2ae942d5b556b7e6bac61dd5e64cbd
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-13 23:57:13 +0530

    List+Oops

--- src/test/java/ListPackage_1/List2.java
+++ src/test/java/ListPackage_1/List2.java
@@ -1,9 +1,31 @@
 package ListPackage_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class List2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		List<String> names=new ArrayList<>();
+		
+		names.add("Future Tense");
+		names.add("Future past");
+		names.add("Future continue");
+		names.add("Past Tense");
+		
+		System.out.println("Names starting with 'F' : ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name=names.get(i);
+			
+			if(name.startsWith("F")) {
+				System.out.println(name);
+			}
+			
+		}
 
 	}
 
---
+++ src/test/java/Oops_Package_13_11_2025/Oops_1.java
@@ -0,0 +1,31 @@
+package Oops_Package_13_11_2025;
+
+public class Oops_1 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void display() {
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Oops_1 op=new Oops_1();
+		op.id=88;
+		op.name="Chaitanya";
+		op.salary=20000;
+		op.display();
+		
+		
+		
+		
+
+	}
+
+}
