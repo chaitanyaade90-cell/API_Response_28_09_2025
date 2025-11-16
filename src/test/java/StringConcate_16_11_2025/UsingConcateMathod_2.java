6e10cc1918e6ff23e75327284052b4671ce712d9
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-16 23:07:55 +0530

    String_2

---
+++ src/test/java/StringConcate_16_11_2025/UsingConcateMathod_2.java
@@ -0,0 +1,17 @@
+package StringConcate_16_11_2025;
+
+public class UsingConcateMathod_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1="Java";
+		String s2="Programming";
+		
+		String output =s1.concat(" ").concat(s2);
+		
+		System.out.println(output);
+
+	}
+
+}
