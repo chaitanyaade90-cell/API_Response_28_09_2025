caa34b0a102bc16ce29e4136168764689bdfaf7f
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-16 17:34:19 +0530

    Nullpointer

---
+++ src/test/java/nullPointer_16_11_2025/NullPointer_3.java
@@ -0,0 +1,20 @@
+package nullPointer_16_11_2025;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NullPointer_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver d1=null;
+		System.out.println("My name is Chaitanya");
+		d1.get("https://www.facebook.com/");
+		
+		System.out.println("After the exception line1");
+		System.out.println("After the exception line2");
+		System.out.println("After the exception line3");
+
+	}
+
+}
