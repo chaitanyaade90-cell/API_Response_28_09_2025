27e15def6bc34353b51cc925e5072fa494910405
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-16 18:18:27 +0530

    Selenium!

---
+++ src/test/java/SeleniumPackage_1/LoginFB_1.java
@@ -0,0 +1,33 @@
+package SeleniumPackage_1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class LoginFB_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriverManager.chromedriver();
+		
+		ChromeDriver d1=new ChromeDriver();
+		
+		d1.get("https://www.facebook.com/");
+		
+		d1.manage().window().maximize();
+		
+		WebElement emailid=d1.findElement(By.id("email"));
+		emailid.sendKeys("chaitanyaade90@gmail.com");
+		
+		WebElement password=d1.findElement(By.id("pass"));
+		password.sendKeys("09chaitanys");
+		
+		WebElement loginbutton=d1.findElement(By.name("login"));
+		loginbutton.click();
+
+	}
+
+}
---
+++ src/test/java/SeleniumPackage_1/package-info.java
@@ -0,0 +1,1 @@
+package SeleniumPackage_1;
