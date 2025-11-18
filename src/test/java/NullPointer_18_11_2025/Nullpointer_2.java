e65cf38fa912e78d313ee34e36330e04d0186d9c
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-18 23:22:53 +0530

    NullPointer_2

---
+++ src/test/java/NullPointer_18_11_2025/Nullpointer_2.java
@@ -0,0 +1,21 @@
+package NullPointer_18_11_2025;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Nullpointer_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver d1=null;
+		System.out.println("My salary is 50000");
+		d1.get("https://www.facebook.com/");
+		
+		System.out.println("Exception on line no 1");
+		System.out.println("Exception on line no 2");
+		System.out.println("Exception on line no 3");
+		
+
+	}
+
+}
