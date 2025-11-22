98ffd36b105025576a73b7bf7f69f4bc8034d477
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-22 10:11:13 +0530

    List1

---
+++ src/test/java/List_22_11_2025/List1.java
@@ -0,0 +1,33 @@
+package List_22_11_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> names=new ArrayList<>();
+		
+		names.add("API");
+		names.add("AXIS");
+		names.add("Archetecture");
+		names.add("Bosch");
+		
+		System.out.println("Names starting with 'A' : ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name=names.get(i);
+			
+			if(name.startsWith("A")) {
+				System.out.println(name);
+			}
+			
+		}
+
+	}
+
+	
+}
