96990441410b374571e32528cdcae3c03fc0abc3
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-22 12:10:12 +0530

    try3

---
+++ src/test/java/TryCatch_package_22_11_2025/TryCatch3.java
@@ -0,0 +1,25 @@
+package TryCatch_package_22_11_2025;
+
+public class TryCatch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try
+		{
+			
+			int num=10/0;
+			
+				
+			}catch (Exception e) {
+				
+				System.out.println("erro occured");
+		
+		
+					
+					
+		}
+
+	}
+
+}
