0757ca2ec55fb5eb0b34cee817c13108bea084de
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-22 11:44:52 +0530

    Concate3

---
+++ src/test/java/StringConcate_22_11_2025/StringConcate_3.java
@@ -0,0 +1,17 @@
+package StringConcate_22_11_2025;
+
+public class StringConcate_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String First="My name is Chaitanya";
+		String Second="I am a Test Engineer";
+		
+		String result=First+" "+Second;
+		
+		System.out.println(result);
+
+	}
+
+}
