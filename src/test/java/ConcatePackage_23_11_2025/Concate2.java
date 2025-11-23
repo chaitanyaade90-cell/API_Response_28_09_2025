2374d9101ec1faeafaee6509fc933198883453dd
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-23 10:27:09 +0530

    Concate2

---
+++ src/test/java/ConcatePackage_23_11_2025/Concate2.java
@@ -0,0 +1,17 @@
+package ConcatePackage_23_11_2025;
+
+public class Concate2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String ProjectName = "TAA";
+		String ProjectRole = "TE";
+		
+		String FUllname= ProjectName+ " " +ProjectRole;
+		
+		System.out.println(FUllname);
+
+	}
+
+}
