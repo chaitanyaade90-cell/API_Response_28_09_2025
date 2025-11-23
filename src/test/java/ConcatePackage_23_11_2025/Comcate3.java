18a8a80a56ed60e95ac080b0388344547b49c443
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-23 11:24:42 +0530

    Concate3

---
+++ src/test/java/ConcatePackage_23_11_2025/Comcate3.java
@@ -0,0 +1,17 @@
+package ConcatePackage_23_11_2025;
+
+public class Comcate3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String fname= "ATR";
+		String lname= "API";
+		
+		String fullname=fname+" "+lname;
+		
+		System.out.println(fullname);
+
+	}
+
+}
