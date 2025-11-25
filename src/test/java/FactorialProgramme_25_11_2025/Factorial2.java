4a8a32eb9e161f812c6b0aa5071fcf5937a5b257
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-25 23:19:17 +0530

    Factorial2

---
+++ src/test/java/FactorialProgramme_25_11_2025/Factorial2.java
@@ -0,0 +1,23 @@
+package FactorialProgramme_25_11_2025;
+
+public class Factorial2 {
+	
+	private static int num;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 5;
+		int fact = 1;
+		
+		for (int i = 1; i <=num; i++) {
+			
+			
+			
+		}
+		
+		System.out.println("Factorial : " + fact);
+
+	}
+
+}
