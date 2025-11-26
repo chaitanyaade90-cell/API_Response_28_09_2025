986e52b0284abf657708d18f1cd7dbca33c87a35
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-11-26 23:51:13 +0530

    Accesss2

---
+++ src/test/java/Acces2.java
@@ -0,0 +1,17 @@
+
+public class Acces2 {
+	
+	public void Dislpay() {
+		
+		System.out.println("My New Car is Range Rover");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Acces2 a1=new Acces2();
+		a1.Dislpay();
+
+	}
+
+}
