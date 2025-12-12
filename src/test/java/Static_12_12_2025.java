c6b4b98bbceea83213ba7c888950e2895f9f70fb
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-12 22:58:35 +0530

    Static1

---
+++ src/test/java/Static_12_12_2025.java
@@ -0,0 +1,35 @@
+
+public class Static_12_12_2025 {
+	
+	static int id;
+	static String name;
+	static int salary;
+	
+	static public void Scrum() {
+		System.out.println("My id is " + id);
+	}
+	
+	static public void Kanben () {
+		System.out.println("My name is " + name);
+	}
+	
+	static public void Lean() {
+		System.out.println("My salary is " + salary);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Static_12_12_2025.id=22;
+		Static_12_12_2025.name="Wipro";
+		Static_12_12_2025.salary=80000;
+		
+		Static_12_12_2025.Scrum();
+		Static_12_12_2025.Kanben();
+		Static_12_12_2025.Lean();
+	
+
+	}
+
+}
