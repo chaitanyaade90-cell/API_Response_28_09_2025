5578a096b0a09b794f74e8c5784e2d9426ce0754
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-12 23:50:03 +0530

    Loop3

---
+++ src/test/java/WhileLoop_12_12_2025/Loop2.java
@@ -0,0 +1,16 @@
+package WhileLoop_12_12_2025;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 2; i < 500; i++) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
---
+++ src/test/java/WhileLoop_12_12_2025/Loop3.java
@@ -0,0 +1,16 @@
+package WhileLoop_12_12_2025;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 3; i < 300; i--) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
