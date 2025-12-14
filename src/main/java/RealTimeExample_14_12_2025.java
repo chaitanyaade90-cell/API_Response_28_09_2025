7f5e1a576314624c1c649cfa7c1f5420facd1b51
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-14 15:45:35 +0530

    RealTime1

---
+++ src/main/java/RealTimeExample_14_12_2025.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class RealTimeExample_14_12_2025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String, String> testCaseStatus =new HashMap<>();
+		
+		testCaseStatus.put("TC_001", "Pass");
+		testCaseStatus.put("TC_002", "Fail");
+		testCaseStatus.put("TC_003", "Blocked");
+		
+		System.out.println(testCaseStatus.get("TC_002"));
+				
+
+	}
+
+}
