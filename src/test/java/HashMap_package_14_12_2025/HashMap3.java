b564050c723f65434b3b089cb7e449a998f3f0dd
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-14 15:24:15 +0530

    HashMap3

---
+++ src/test/java/HashMap_package_14_12_2025/HashMap3.java
@@ -0,0 +1,21 @@
+package HashMap_package_14_12_2025;
+
+import java.util.HashMap;
+
+public class HashMap3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map=new HashMap<>();
+		
+		map.put(8, "ATRService");
+		map.put(10, "RunTest");
+		map.put(11, "SearchResult");
+		map.put(9, "ATRService");
+		
+		System.out.println(map);
+
+	}
+
+}
