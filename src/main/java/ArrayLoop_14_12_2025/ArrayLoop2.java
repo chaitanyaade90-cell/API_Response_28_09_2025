d4a1aa2fef65102ca8e9f70a1380a8ecb741580b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-14 14:43:17 +0530

    ArryLoop3

---
+++ src/main/java/ArrayLoop_14_12_2025/ArrayLoop2.java
@@ -0,0 +1,18 @@
+package ArrayLoop_14_12_2025;
+
+public class ArrayLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numbers[] = {10,20,30};
+		
+		for (int i = 0; i < numbers.length; i++) {
+			
+			System.out.println(numbers[i]);
+			
+		}
+
+	}
+
+}
---
+++ src/main/java/ArrayLoop_14_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package ArrayLoop_14_12_2025;
