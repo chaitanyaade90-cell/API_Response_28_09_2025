516e506cceb618995d36e3b3a72de3b51aaaa043
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-14 15:18:10 +0530

    HasMap2

---
+++ src/test/java/HashMap_package_14_12_2025/HashMap2.java
@@ -0,0 +1,20 @@
+package HashMap_package_14_12_2025;
+
+import java.util.HashMap;
+
+public class HashMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(4, "BlackBoxTesting");
+		map.put(5, "WhiteBoxTesting");
+		map.put(6, "GrayBoxTesting");
+		
+		System.out.println(map);
+
+	}
+
+}
