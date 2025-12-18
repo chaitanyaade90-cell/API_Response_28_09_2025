917572c915a14d28e7d68eb18df133db4f61f3aa
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-18 22:17:44 +0530

    ins2

---
+++ src/test/java/Instance_package_18_12_2025/Instance2.java
@@ -0,0 +1,36 @@
+package Instance_package_18_12_2025;
+
+public class Instance2 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void ATR() {
+		System.out.println("My id is " + id);
+	}
+	
+	public void RunTest() {
+		System.out.println("My name is " + name);
+	}
+	
+	public void UniversalAutomation() {
+		System.out.println("My salary is " + salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Instance2 i2=new Instance2();
+		
+		i2.id=90;
+		i2.name="AxisBank";
+		i2.salary=90000;
+		
+		i2.ATR();
+		i2.RunTest();
+		i2.UniversalAutomation();
+
+	}
+
+}
