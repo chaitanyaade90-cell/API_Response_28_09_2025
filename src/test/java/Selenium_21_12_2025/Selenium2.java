320ee742ac31f34c4d153dad8d932a0fd516421b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-21 23:52:36 +0530

    Login2

---
+++ src/test/java/Selenium_21_12_2025/Selenium2.java
@@ -0,0 +1,35 @@
+package Selenium_21_12_2025;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1=new ChromeDriver();
+		
+		d1.get("https://www.facebook.com/");
+		d1.manage().window().maximize();
+		
+		WebElement emailid=d1.findElement(By.id("email"));
+		emailid.sendKeys("chaitanyaade90@gmail.com");
+		
+		WebElement password=d1.findElement(By.id("pass"));
+		password.sendKeys("09chaitanys");
+		
+		WebElement loginid=d1.findElement(By.name("login"));
+		loginid.click();
+		
+		
+		
+
+	}
+
+}
