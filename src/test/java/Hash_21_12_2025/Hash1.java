a89592f6126db23c86346d45227add4934c7d634
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-21 22:48:42 +0530

    Hash1

---
+++ src/test/java/Hash_21_12_2025/Hash1.java
@@ -0,0 +1,21 @@
+package Hash_21_12_2025;
+
+import java.util.HashMap;
+
+public class Hash1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map=new HashMap<>();
+		
+		map.put(1, "BlackBox");
+		map.put(2, "WhiteBox");
+		map.put(3, "GrayBox");
+		
+		System.out.println(map);
+		
+
+	}
+
+}
