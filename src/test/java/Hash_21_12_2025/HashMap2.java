3c2127a144cd2612034e96112cde8c4143c6027e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-21 22:59:29 +0530

    Hash2

---
+++ src/test/java/Hash_21_12_2025/HashMap2.java
@@ -0,0 +1,21 @@
+package Hash_21_12_2025;
+
+import java.util.HashMap;
+
+public class HashMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map=new HashMap();
+		
+		map.put(11, "SmokeTesting");
+		map.put(12, "FunctionalTesting");
+		map.put(33, "Software");
+		map.put(11, "SmokeTesting");
+		
+		System.out.println(map);
+
+	}
+
+}
