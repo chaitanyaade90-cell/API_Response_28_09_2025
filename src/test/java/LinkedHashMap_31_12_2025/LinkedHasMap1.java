a7ddf3ba68d93f56514c3be88ef5a1172cf98c24
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-31 18:30:09 +0530

    HashMap1

---
+++ src/test/java/LinkedHashMap_31_12_2025/LinkedHasMap1.java
@@ -0,0 +1,21 @@
+package LinkedHashMap_31_12_2025;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHasMap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<Integer, String> map = new LinkedHashMap<>();
+		
+		map.put(3, "Orange");
+		map.put(4, "Apple");
+		map.put(5, "Banana");
+		
+		System.out.println("LinkedMap: " + map);
+
+	}
+
+}
---
+++ src/test/java/LinkedHashMap_31_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package LinkedHashMap_31_12_2025;
