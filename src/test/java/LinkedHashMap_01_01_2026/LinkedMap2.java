f313b697d36bee200fd3cdf7193e594a9dd77743
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-01 13:39:39 +0530

    Map2

---
+++ src/test/java/LinkedHashMap_01_01_2026/LinkedMap2.java
@@ -0,0 +1,21 @@
+package LinkedHashMap_01_01_2026;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<Integer, String> map = new LinkedHashMap<>();
+		
+		map.put(200, "NFR");
+		map.put(300, "Onboarding");
+		map.put(400, "TestAuto");
+		
+		System.out.println("LinkedHashMap : " + map);
+
+	}
+
+}
