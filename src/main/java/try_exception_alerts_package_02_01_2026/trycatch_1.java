3535692538adbf2c41c822c8f7c39d8979e5e72a
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-02 15:51:46 +0530

    trycatch1

---
+++ src/main/java/try_exception_alerts_package_02_01_2026/trycatch_1.java
@@ -0,0 +1,19 @@
+package try_exception_alerts_package_02_01_2026;
+
+public class trycatch_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			int a= 10/0;
+			
+		}catch (ArithmeticException e) {
+			
+			System.out.println(" Cant division by 0");
+			
+		}
+
+	}
+
+}
