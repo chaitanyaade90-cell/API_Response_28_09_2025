25c9803e2e0fe9141d708b0d30575da7973edecf
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-02 17:27:48 +0530

    brokenlinks1

---
+++ src/test/java/BrokenLink_02_01_2026/BrokenLinks1.java
@@ -0,0 +1,37 @@
+package BrokenLink_02_01_2026;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrokenLinks1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.gmail.com");
+		
+		WebElement links = d1.findElement(By.tagName("aa"));
+		
+		
+			
+			String url = links.getAttribute("href");
+			
+			if(url==null) url.isEmpty();
+		}
+			
+		}
+		
+		
+
+	}
+
+}
---
+++ src/test/java/BrokenLink_02_01_2026/package-info.java
@@ -0,0 +1,1 @@
+package BrokenLink_02_01_2026;
