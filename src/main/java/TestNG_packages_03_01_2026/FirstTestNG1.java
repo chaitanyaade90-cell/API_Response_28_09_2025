fa563280565a278e4716be87a2ba73f5d188c084
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-03 14:54:04 +0530

    Logout1

---
+++ src/main/java/TestNG_packages_03_01_2026/FirstTestNG1.java
@@ -0,0 +1,20 @@
+package TestNG_packages_03_01_2026;
+
+import org.junit.Test;
+
+public class FirstTestNG1 {
+	
+	@Test 
+	public void LoginTest() {
+		
+		System.out.println("Test Login Executed");
+	}
+	
+	@Test
+	public void LogoutTest() {
+		
+		System.out.println("Logout test executed");
+	}
+	
+
+}
---
+++ src/main/java/TestNG_packages_03_01_2026/Test.java
@@ -0,0 +1,5 @@
+package TestNG_packages_03_01_2026;
+
+public @interface Test {
+
+}
