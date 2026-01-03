2f45a9032b720f6fc0393e17a7ff58d4d5c4a950
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-03 14:38:51 +0530

    SetClass1

---
+++ src/main/java/SetPackages_03_01_2026/SetClass1.java
@@ -0,0 +1,22 @@
+package SetPackages_03_01_2026;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<String> set = new HashSet<>();
+		
+		set.add("Java");
+		set.add("Selenium");
+		set.add("Java");
+		
+		System.out.println(set);
+		
+
+	}
+
+}
---
+++ src/main/java/SetPackages_03_01_2026/package-info.java
@@ -0,0 +1,1 @@
+package SetPackages_03_01_2026;
