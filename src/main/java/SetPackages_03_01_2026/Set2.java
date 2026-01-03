71692cdb20279724e15ecdde87820cb0218047c9
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-03 14:41:21 +0530

    set2

---
+++ src/main/java/SetPackages_03_01_2026/Set2.java
@@ -0,0 +1,22 @@
+package SetPackages_03_01_2026;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<String> set = new HashSet<>();
+		
+		set.add("ADO");
+		set.add("Agile");
+		set.add("Inheritance");
+		set.add("ADO");
+		
+		System.out.println(set);
+
+	}
+
+}
