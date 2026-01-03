74ed68523106c552ac7d4bd8b64ff18e7f57c136
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-01-03 15:44:48 +0530

    ListInt

---
+++ src/main/java/ListPackage_03_01_2026/ListElement1.java
@@ -0,0 +1,32 @@
+package ListPackage_03_01_2026;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListElement1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Integer> numbers = new ArrayList<>();
+		
+		numbers.add(1000);
+		numbers.add(2000);
+		numbers.add(3000);
+		
+		System.out.println(numbers.get(1)); //20
+		
+		
+		
+		
+			
+			}
+				
+			
+			
+		
+		
+
+	}
+
+
