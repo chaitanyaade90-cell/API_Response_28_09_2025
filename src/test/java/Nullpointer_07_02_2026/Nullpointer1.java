ae0ff4c4c46e216249e1fb64c092584aa82cf6e0
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-07 13:38:53 +0530

    Null1

---
+++ src/test/java/Nullpointer_07_02_2026/Nullpointer1.java
@@ -0,0 +1,20 @@
+package Nullpointer_07_02_2026;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Nullpointer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver d1=null;
+		System.out.println("My company name is Coforge");
+		d1.get("https://www.gmail.com");
+		
+		System.out.println("Exception on line no 1");
+		System.out.println("Exception on line no 2");
+		System.out.println("Exception on line no 3");
+
+	}
+
+}
---
+++ src/test/java/Nullpointer_07_02_2026/package-info.java
@@ -0,0 +1,1 @@
+package Nullpointer_07_02_2026;
