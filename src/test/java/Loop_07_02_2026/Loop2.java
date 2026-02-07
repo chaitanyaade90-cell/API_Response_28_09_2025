1911b51f9a89208fa03f935c9d4152ad433aef23
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-07 16:34:27 +0530

    Loop2

---
+++ src/test/java/Loop_07_02_2026/Loop2.java
@@ -0,0 +1,16 @@
+package Loop_07_02_2026;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 1000; i > 800; i--) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
