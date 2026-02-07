f01074b69f858b55cde3d392bd12596e5afac2c5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-08 00:15:30 +0530

    concate3

---
+++ src/test/java/Concate_07_02_2026/Concate3.java
@@ -0,0 +1,17 @@
+package Concate_07_02_2026;
+
+public class Concate3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String first = "Onboarding";
+		String last = "Search result";
+		
+		String result = first +"  "+ last;
+		
+		System.out.println(result);
+
+	}
+
+}
