76fdcf143223f4b71b468065089cf6ad9af1a053
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-07 16:29:06 +0530

    hashmap2

---
+++ src/test/java/HashList_07_02_2026/HashMap2.java
@@ -0,0 +1,20 @@
+package HashList_07_02_2026;
+
+import java.util.HashMap;
+
+public class HashMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> name = new HashMap<>();
+		
+		name.put(900, "Canon");
+		name.put(800, "Dell");
+		name.put(700, "Dutch");
+		
+		System.out.println(name);
+
+	}
+
+}
