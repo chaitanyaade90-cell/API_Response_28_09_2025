a278a1295eb1d767f65b978b917f5d30d6968ee6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-09 23:11:49 +0530

    hashmap2

--- src/test/java/HasMap_09_02_2026/HashMap2.java
+++ src/test/java/HasMap_09_02_2026/HashMap2.java
@@ -2,19 +2,18 @@ package HasMap_09_02_2026;
 
 import java.util.HashMap;
 
-public class Hashmap1 {
+public class HashMap2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		HashMap<Integer, String> map = new HashMap<>();
 		
-		map.put(2000, "RunTest");
-		map.put(3000,"SearchReuslt");
-		map.put(4000,"Onboarding");
+		map.put(900, "Searchresult");
+		map.put(1000, "E2E");
+		map.put(700, "UniversalTest");
 		
 		System.out.println(map);
-		
 
 	}
 
