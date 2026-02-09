7aba836de06616edda27b93f8bb86e00a72a07e0
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-09 22:53:43 +0530

    List1

---
+++ src/test/java/List_09_02_2026/List1.java
@@ -0,0 +1,30 @@
+package List_09_02_2026;
+
+import java.util.ArrayList;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> names = new ArrayList<>();
+		
+		names.add("Regression");
+		names.add("RestAssured");
+		names.add("Restore");
+		
+		System.out.println("Names starting with 'R' ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name= names.get(i);
+			
+			if(name.startsWith("R")) {
+				System.out.println(name);
+			}
+			
+		}
+
+	}
+
+}
---
+++ src/test/java/StringClass_08_02_2026/StringClass2.java
@@ -0,0 +1,18 @@
+package StringClass_08_02_2026;
+
+public class StringClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "NonFunctionalTesting";
+		
+		for (int i = 0; i < name.length(); i++) {
+			
+			System.out.println(name.charAt(i));
+			
+		}
+
+	}
+
+}
