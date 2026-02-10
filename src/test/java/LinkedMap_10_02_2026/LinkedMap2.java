b518a49d331f34e925724ba63122a981a9ea57c8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-10 23:57:26 +0530

    linkd2

---
+++ src/test/java/LinkedMap_10_02_2026/LinkedMap2.java
@@ -0,0 +1,21 @@
+package LinkedMap_10_02_2026;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<Integer, String> map = new LinkedHashMap<>();
+		
+		map.put(1000, "Onboarding");
+		map.put(2000, "VSCODE");
+		map.put(3000, "E2E");
+		
+		System.out.println("LinkedHasMap: " + map);
+
+	}
+
+}
