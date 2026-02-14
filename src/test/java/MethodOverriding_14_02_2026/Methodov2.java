c2ae0bda8f79b719ba5b1a8224109cfd522a061e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-14 16:19:03 +0530

    Mthodov2

---
+++ src/test/java/MethodOverriding_14_02_2026/Methodov2.java
@@ -0,0 +1,31 @@
+package MethodOverriding_14_02_2026;
+
+class calculator
+{
+	
+	int add(int a, int b) {
+		return a+b;
+		
+	}
+	
+	int add(int a, int b, int c) {
+		return a+b+c;
+				
+	}
+}
+
+public class Methodov2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		calculator c1 = new calculator();
+		
+		System.out.println(c1.add(1000, 2000));
+		System.out.println(c1.add(3000, 4000, 5000));
+		
+		
+
+	}
+
+}
