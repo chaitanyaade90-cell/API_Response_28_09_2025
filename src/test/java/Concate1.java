42a2d3b5246ed3e96bb470d879193c6b2a0260b6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-14 14:24:30 +0530

    Concate1

---
+++ src/test/java/Concate1.java
@@ -0,0 +1,15 @@
+
+public class Concate1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String city = "Pune";
+		int Pin = 411014;
+		
+		System.out.println("City: " + city + ", PIN: " 	+ Pin);
+		
+
+	}
+
+}
---
+++ src/test/java/Concate_14_02_2026.java
@@ -0,0 +1,9 @@
+
+public class Concate_14_02_2026 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
