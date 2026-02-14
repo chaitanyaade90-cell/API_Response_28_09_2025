3b788c7e4eb166343806151d815ee1387ae7e719
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-14 14:35:40 +0530

    COncate2

---
+++ src/test/java/Concate2.java
@@ -0,0 +1,15 @@
+
+public class Concate2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String City = "Pune";
+		
+		int Pin = 411014;
+		
+		System.out.println("City: " + City + ", PIN: " + Pin );
+
+	}
+
+}
