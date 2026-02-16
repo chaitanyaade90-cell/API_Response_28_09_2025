73d0c3c9c4360d34b623b07c5d9d09a0dab10fae
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-16 22:54:41 +0530

    List1

---
+++ src/test/java/List_16_02_2026/List1.java
@@ -0,0 +1,31 @@
+package List_16_02_2026;
+
+import java.util.ArrayList;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> names = new ArrayList<>();
+		
+		names.add("String");
+		names.add("Severity");
+		names.add("Software");
+		
+		System.out.println("Names starting with 'S' ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name = names.get(i);
+			
+			if(name.startsWith("S")) {
+				
+				System.out.println(name);
+			}
+			
+		}
+
+	}
+
+}
