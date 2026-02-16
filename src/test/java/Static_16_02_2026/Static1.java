6d9e7a254e6e6c902967e8bbe3dac0f8f833ad72
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-16 22:40:34 +0530

    static1

---
+++ src/test/java/Static_16_02_2026/Static1.java
@@ -0,0 +1,37 @@
+package Static_16_02_2026;
+
+public class Static1 {
+	
+	static int id;
+	static String name;
+	static int salary;
+	
+	public static void VSCode() {
+		
+		System.out.println("My id is " + id);
+	}
+	
+	public static void Xpath() {
+		
+		System.out.println("My name is " + name);
+	}
+	
+	public static void CSS() {
+		
+		System.out.println("My salary is " + salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Static1.id=11;
+		Static1.name="NFR";
+		Static1.salary=90000;
+		
+		Static1.VSCode();
+		Static1.Xpath();
+		Static1.CSS();
+
+	}
+
+}
---
+++ src/test/java/Static_16_02_2026/package-info.java
@@ -0,0 +1,1 @@
+package Static_16_02_2026;
