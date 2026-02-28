67b59441fdd31dd8335a21ef7549a1b0e2a5b715
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-02-28 16:41:04 +0530

    Concate2

---
+++ src/test/java/Concate_28_02_2026/Concate2.java
@@ -0,0 +1,17 @@
+package Concate_28_02_2026;
+
+public class Concate2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String First = "AzureDevOps";
+		String Last = "SoftwareTesting";
+		
+		String result = First +" " + Last;
+		
+		System.out.println(result);
+
+	}
+
+}
