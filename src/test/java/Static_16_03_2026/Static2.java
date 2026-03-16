a7636243a71a617b574c7fb0b5b312b61598a323
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-03-16 22:59:01 +0530

    Static2

---
+++ src/test/java/Static_16_03_2026/Static2.java
@@ -0,0 +1,38 @@
+package Static_16_03_2026;
+
+public class Static2 {
+	
+	static int number;
+	static String name;
+	static int salary;
+	
+	public static void SQMMinion() {
+		
+		System.out.println("My number is " + number);
+	}
+	
+	public static void VSCode() {
+		
+		System.out.println("My name is " + name);
+	}
+	
+	public static void MSSQL() {
+		
+		System.out.println("My salary is " + salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Static2.number=11;
+		Static2.name="Courage";
+		Static2.salary=90000;
+		
+		Static2.SQMMinion();
+		Static2.VSCode();
+		Static2.MSSQL();
+		
+
+	}
+
+}
