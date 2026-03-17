17206d08a072aa48705d5cc37f53d2ba9416cbb5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-03-17 07:39:06 +0530

    oper2

---
+++ src/test/java/Operators_package_17_03_2026/Operators2.java
@@ -0,0 +1,24 @@
+package Operators_package_17_03_2026;
+
+public class Operators2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	// Operators
+		
+		// Assingements
+		
+		int a = 0;
+		
+		a = 100;
+		boolean b = false;
+		b = true;
+		
+		a= 100;
+		
+		System.out.println(b);
+
+	}
+
+}
