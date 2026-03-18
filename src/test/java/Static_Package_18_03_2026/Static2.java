5d6414bc745dda81922741e2a0c9ab116c6bb283
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-03-18 22:43:42 +0530

    static2

--- src/test/java/Static_Package_18_03_2026/Static2.java
+++ src/test/java/Static_Package_18_03_2026/Static2.java
@@ -1,9 +1,35 @@
 package Static_Package_18_03_2026;
 
 public class Static2 {
+	
+	static int number;
+	static String name;
+	static int salary;
+	
+	public static void RunTest() {
+		
+		System.out.println("My number is " + number);
+	}
+	
+	public static void SQM() {
+		
+		System.out.println("My name is " + name);
+	}
 
+	public static void VSCode() {
+		
+		System.out.println("My salary is " + salary);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Static2.number=99;
+		Static2.name="Coforge";
+		Static2.salary=100000;
+		
+		Static2.RunTest();
+		Static2.SQM();
+		Static2.VSCode();
 
 	}
 
