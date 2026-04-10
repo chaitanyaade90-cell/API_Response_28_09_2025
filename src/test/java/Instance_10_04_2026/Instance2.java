844387eaeef36ebabe07d4e999a2e1665b81c721
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-10 22:39:10 +0530

    inst2

---
+++ src/test/java/Instance_10_04_2026/Instance2.java
@@ -0,0 +1,39 @@
+package Instance_10_04_2026;
+
+public class Instance2 {
+	
+	int number;
+	String name;
+	int salary;
+	
+	public void Smoke() {
+		
+		System.out.println("My number is " + number);
+	}
+	
+	public void Sanity() {
+		
+		System.out.println("My name is " + name);
+	}
+	
+	public void Regression() {
+		
+		System.out.println("My salary is " + salary);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Instance2 i2 = new Instance2();
+		
+		i2.number=99;
+		i2.name="Axa";
+		i2.salary=100000;
+		
+		i2.Smoke();
+		i2.Sanity();
+		i2.Regression();
+
+	}
+
+}
