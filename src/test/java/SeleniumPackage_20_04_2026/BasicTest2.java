5f284aaadbd0b2e937761508f9cdb2098e3a6cc8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-20 22:10:07 +0530

    Basic2

---
+++ src/test/java/SeleniumPackage_20_04_2026/BasicTest2.java
@@ -0,0 +1,25 @@
+package SeleniumPackage_20_04_2026;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasicTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.google.com");
+		
+		String title = d1.getTitle();
+		
+		System.out.println("Page Title " + title);
+		
+
+	}
+
+}
