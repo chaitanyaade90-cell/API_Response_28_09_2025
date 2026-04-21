88252b6d6acfad108433f399e8fee38d8384cd49
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-21 22:25:34 +0530

    btn2

---
+++ src/test/java/ClickButtonPackage_21_04_2026/ButtonC2.java
@@ -0,0 +1,25 @@
+package ClickButtonPackage_21_04_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ButtonC2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.examples.com");
+		
+		d1.findElement(By.id("btn")).click();
+		
+		
+
+	}
+
+}
