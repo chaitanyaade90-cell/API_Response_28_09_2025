3efbb39a1982635a19af2ed9814663321190d9a5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-23 22:48:40 +0530

    cssselect2

---
+++ src/test/java/CSSSelector_Package_23_04_2026/CssSelector2.java
@@ -0,0 +1,27 @@
+package CSSSelector_Package_23_04_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CssSelector2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.examples.com");
+		
+		d1.findElement(By.cssSelector("username")).sendKeys("admin");
+		
+		d1.findElement(By.cssSelector("password")).sendKeys("password");
+		
+		d1.findElement(By.cssSelector("buttonlogin")).click();
+
+	}
+
+}
