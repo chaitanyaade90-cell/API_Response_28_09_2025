ac686a261608efb4c42e0fa95e16013da14fab01
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-25 14:41:42 +0530

    alertConfirm1

---
+++ src/test/java/Package_25_04_2026/AlertOk_1.java
@@ -0,0 +1,28 @@
+package Package_25_04_2026;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class AlertOk_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://demoqa.com/alerts");
+		
+		Alert alert = d1.switchTo().alert();
+		
+		alert.dismiss();
+		
+		
+		
+
+	}
+
+}
