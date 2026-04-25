974284127e84d806520d0202124c65290e516d75
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-25 16:23:45 +0530

    inherit1

---
+++ src/test/java/Package_25_04_2026/Inheritance1.java
@@ -0,0 +1,32 @@
+package Package_25_04_2026;
+
+class Xpath
+{
+	
+	public void Symantec() {
+		
+		System.out.println("My Role is PTM");
+	}
+}
+
+class CssSelector extends Xpath{
+	
+	public void Xviour() {
+		
+		System.out.println("My Designamtion is QE");
+	}
+}
+
+public class Inheritance1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CssSelector cs = new CssSelector();
+		
+		cs.Symantec();
+		cs.Xviour();
+
+	}
+
+}
