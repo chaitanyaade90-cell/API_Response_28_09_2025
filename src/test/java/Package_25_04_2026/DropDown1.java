08d4283382917b569d614134b4ae935d2060b777
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-25 15:46:04 +0530

    dropdown1

---
+++ src/test/java/Package_25_04_2026/DropDown1.java
@@ -0,0 +1,28 @@
+package Package_25_04_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DropDown1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.example.com");
+		
+		Select dropdown = new Select(d1.findElement(By.id("country")));
+		
+		dropdown.selectByValue("In");
+		
+		dropdown.selectByIndex(1);
+
+	}
+
+}
