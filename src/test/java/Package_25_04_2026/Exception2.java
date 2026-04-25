c0e788c5f946fde607116c12ef34b81b53d7d3a8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-25 12:41:12 +0530

    exception2

---
+++ src/test/java/Package_25_04_2026/Exception2.java
@@ -0,0 +1,17 @@
+package Package_25_04_2026;
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100;
+		int b = 200;
+		
+		int result = a/b; // exception alerts
+		
+		System.out.println(result);
+
+	}
+
+}
