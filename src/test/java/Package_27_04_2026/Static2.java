bb269250f7e8752dcf77d5fc39d517a77721679b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-27 23:26:06 +0530

    static2

---
+++ src/test/java/Package_27_04_2026/Static2.java
@@ -0,0 +1,39 @@
+package Package_27_04_2026;
+
+public class Static2 {
+	
+	static int id;
+	static String name;
+	static int salary;
+	
+	public static void Software() {
+		
+		System.out.println("My id is " + id);
+	}
+	
+	public static void Hardware() {
+		
+		System.out.println("My name is " + name);
+	}
+	
+	public static void Linkdin() {
+		
+		System.out.println("My salary is ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Static2.id=11;
+		Static2.name="Phoenix";
+		Static2.salary=200000;
+		
+		Static2.Hardware();
+		Static2.Linkdin();
+		Static2.Software();
+		
+
+	}
+
+}
