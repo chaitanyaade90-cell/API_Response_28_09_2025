e010a57b0ac5679df3837cb5f17b51b552c20ba8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-28 23:11:55 +0530

    exception1

---
+++ src/main/java/Package_28_04_2026/ExceptionTest1.java
@@ -0,0 +1,31 @@
+package Package_28_04_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExceptionTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://example.com");
+		
+		try {
+			
+			d1.findElement(By.id("wrongId")).click();
+			
+		} catch (Exception e) {
+			
+			System.out.println("Element ");
+		}
+		
+
+	}
+
+}
