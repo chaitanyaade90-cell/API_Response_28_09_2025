0030e964566534a62a13813d5a8ab6b03c57e9c6
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-04-28 23:17:17 +0530

    exception2

--- src/main/java/Package_28_04_2026/Exception2.java
+++ src/main/java/Package_28_04_2026/Exception2.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class ExceptionTest1 {
+public class Exception2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,7 +14,7 @@ public class ExceptionTest1 {
 		
 		ChromeDriver d1 = new ChromeDriver();
 		
-		d1.get("https://example.com");
+		d1.get("https://www.example.com");
 		
 		try {
 			
@@ -24,7 +24,6 @@ public class ExceptionTest1 {
 			
 			System.out.println("Element");
 		}
-		
 
 	}
 
