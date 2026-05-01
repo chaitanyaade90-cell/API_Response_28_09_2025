20a8d2b7e6ff1e00b1ffebfb34640c5c7fc14547
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 13:43:17 +0530

    hash2

---
+++ src/test/java/Package_01_05_2026/Hashmap2.java
@@ -0,0 +1,22 @@
+package Package_01_05_2026;
+
+import java.util.HashMap;
+
+public class Hashmap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(4000, "SearchResult");
+		map.put(5000, "SQMMIon");
+		map.put(6000, "MsSql");
+		
+		System.out.println(map);
+		
+		
+
+	}
+
+}
