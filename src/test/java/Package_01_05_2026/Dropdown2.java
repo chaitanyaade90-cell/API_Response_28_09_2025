5ced2d225b2fea9ed7c3908e38a739962129eb3f
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 15:34:53 +0530

    dropdown2

---
+++ src/test/java/Package_01_05_2026/Dropdown2.java
@@ -0,0 +1,26 @@
+package Package_01_05_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Dropdown2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.example.com");
+		
+		Select dropdown = new Select(d1.findElement(By.id("india")));
+		
+		dropdown.selectByVisibleText("India");
+
+	}
+
+}
