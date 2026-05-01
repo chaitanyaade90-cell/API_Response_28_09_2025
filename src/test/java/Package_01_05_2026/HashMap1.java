45fce98696d6dc39f0fb76fa2b119062b7e168f7
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 13:40:36 +0530

    hashmap1

---
+++ src/test/java/Package_01_05_2026/HashMap1.java
@@ -0,0 +1,22 @@
+package Package_01_05_2026;
+
+import java.util.HashMap;
+
+public class HashMap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(1000, "RunTest");
+		map.put(2000, "NFR");
+		map.put(3000, "Onboarding");
+		
+		System.out.println(map);
+		
+		
+
+	}
+
+}
