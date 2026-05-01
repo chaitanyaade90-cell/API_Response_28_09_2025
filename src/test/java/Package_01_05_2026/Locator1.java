68c48d3ef1e81581fcd3c2822609975e8fadbe90
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 14:51:58 +0530

    locator1

---
+++ src/test/java/Package_01_05_2026/Locator1.java
@@ -0,0 +1,28 @@
+package Package_01_05_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locator1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.example.com");
+		
+		d1.findElement(By.id("username")).sendKeys("admin");
+		d1.findElement(By.id("password")).sendKeys("12345");
+		
+		d1.findElement(By.xpath("//button[text()='Login]")).click();
+		
+		
+
+	}
+
+}
