631be46f8d5809d5f864376e047480be8b154f66
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 20:34:56 +0530

    even1

---
+++ src/test/java/Package_01_05_2026/Array_Even_num.java
@@ -0,0 +1,21 @@
+package Package_01_05_2026;
+
+public class Array_Even_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] arr = {1,2,3,4,5};
+		
+		for(int num : arr) {
+			
+				if(num % 2 == 0) {
+					
+					System.out.println(num);
+					
+				}
+		}
+
+	}
+
+}
