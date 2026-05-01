0ec4974c2387f909f0dc340fb75c1a6d8eb69721
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 20:29:06 +0530

    forloop1

---
+++ src/test/java/Package_01_05_2026/ArrayLoop1.java
@@ -0,0 +1,18 @@
+package Package_01_05_2026;
+
+public class ArrayLoop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {5,10,20};
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
