83654d16348ee8f59a37275e62330a2e2fb0af0d
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-01 16:21:39 +0530

    claickbtn1

---
+++ src/test/java/Package_01_05_2026/ClickButton1.java
@@ -0,0 +1,31 @@
+package Package_01_05_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ClickButton1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.findElement(By.id("username")).sendKeys("admin");
+		
+		d1.findElement(By.id("password")).sendKeys("12345");
+		
+		d1.get("https://www.example.com");
+		
+		d1.findElement(By.id("loginBtn")).click();
+		
+		
+		
+		
+
+	}
+
+}
