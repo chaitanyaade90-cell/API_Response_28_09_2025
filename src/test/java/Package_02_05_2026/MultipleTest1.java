75e5d7315bd0adcb8e5f2beb35efd8cc32d27678
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-02 16:12:08 +0530

    Multiple

---
+++ src/test/java/Package_02_05_2026/MultipleTest1.java
@@ -0,0 +1,24 @@
+package Package_02_05_2026;
+
+import org.testng.annotations.Test;
+
+public class MultipleTest1 {
+	
+	@Test
+	public void login() {
+		
+		System.out.println("Login Test");
+		
+	}
+	
+	@Test
+	public void dashboard() {
+		
+		System.out.println("Dashboard Test");
+	}
+
+	
+
+	}
+
+
