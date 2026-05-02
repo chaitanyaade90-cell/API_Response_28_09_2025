480d7619b9ac8bbd0ed0c9144cc861a0e708be57
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-02 15:56:33 +0530

    Method

---
+++ src/test/java/Package_02_05_2026/MethodArgument1.java
@@ -0,0 +1,24 @@
+package Package_02_05_2026;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MethodArgument1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		}
+	
+	public void login(String username, String password) {
+		
+	}
+
+	}
+
+
