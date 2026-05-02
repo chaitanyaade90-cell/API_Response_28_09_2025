522fd945ea7bff114e82a5185acc92b31cc05a25
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-02 17:07:12 +0530

    inherit1

---
+++ src/test/java/Package_02_05_2026/Inheritance1.java
@@ -0,0 +1,35 @@
+package Package_02_05_2026;
+
+class Bombay
+{
+	
+	public void City() {
+		
+		System.out.println("Bombay is biggest city in indai");
+	}
+	
+}
+
+class Newyork extends Bombay{
+	
+	public void Demography() {
+		
+	}
+	}
+
+
+public class Inheritance1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub//
+		
+		Newyork ny = new Newyork();
+		
+		ny.City();
+		ny.Demography();
+				
+				
+
+	}
+
+}
