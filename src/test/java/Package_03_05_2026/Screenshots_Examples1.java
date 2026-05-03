d161ca56673bc1bcc39b3f6e3ff1945b35b760e4
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 14:05:44 +0530

    screenshot2

---
+++ src/test/java/Package_03_05_2026/Screenshots_Examples1.java
@@ -0,0 +1,34 @@
+package Package_03_05_2026;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Screenshots_Examples1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.saucedemo.com/");
+		
+		TakesScreenshot ts = (TakesScreenshot) d1;
+		
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		
+		FileHandler.copy(src, new File("screenshots.png"));
+		
+		
+
+	}
+
+}
