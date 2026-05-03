e79c5e6b8a971237db5af6de75e89834e244e3c5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 22:50:21 +0530

    oddeven

--- src/test/java/Package_03_05_2026/OddEven1.java
+++ src/test/java/Package_03_05_2026/OddEven1.java
@@ -4,6 +4,16 @@ public class OddEven1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int num = 7;
+		
+		if(num % 2 == 0);{
+			
+			System.out.println("Even number");
+		
+		
+			System.out.println("odd number");
+		}
 
 	}
 
