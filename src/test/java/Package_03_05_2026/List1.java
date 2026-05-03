9ab0c7abf777d0c22976bc48d529b54a033162bc
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 18:11:27 +0530

    list1

---
+++ src/test/java/Package_03_05_2026/List1.java
@@ -0,0 +1,32 @@
+package Package_03_05_2026;
+
+import java.util.ArrayList;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> names = new ArrayList<>();
+		
+		names.add("Addition");
+		names.add("Abstarct");
+		names.add("Adverb");
+		
+		System.out.println("Names starting with 'A' ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name = names.get(i);
+			
+			if(name.startsWith("A")) {
+				System.out.println(name);
+			}
+			
+		}
+		
+		
+
+	}
+
+}
