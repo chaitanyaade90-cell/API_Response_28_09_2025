4e34dce5e5bf9c2147666b0c829b6d465a7356a0
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 22:16:47 +0530

    ifesle_if

---
+++ src/test/java/Package_03_05_2026/If_else_if.java
@@ -0,0 +1,28 @@
+package Package_03_05_2026;
+
+public class If_else_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int marks = 75;
+		
+		if(marks >90) {
+			
+			System.out.println("Grade A");
+		
+		}else if (marks >= 60) {
+			
+			System.out.println("Grade B");
+			
+		}else if (marks >=40) {
+			System.out.println("Grade C");
+		
+		}else {
+			System.out.println("Fail");
+			
+		}
+
+	}
+
+}
