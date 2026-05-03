db384dc043c71a90b6f1fcea1640bf8a9de3b39e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 12:05:46 +0530

    hashmap1

---
+++ src/test/java/Package_03_05_2026/Hashmap1.java
@@ -0,0 +1,23 @@
+package Package_03_05_2026;
+
+import java.util.HashMap;
+
+public class Hashmap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String, String> map = new HashMap<>();
+		
+		map.put("name", "chaitanya");
+		map.put("process", "Manual");
+		map.put("role", "ptm");
+		
+		System.out.println(map);
+		
+		System.out.println(map.get("name"));
+				
+
+	}
+
+}
