a41e2984d203f3d16e37d665911d40939c53a2d3
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 22:34:55 +0530

    LoginCheck

---
+++ src/test/java/Package_03_05_2026/LoginCheck.java
@@ -0,0 +1,20 @@
+package Package_03_05_2026;
+
+public class LoginCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Username = "admin";
+		String password = "1234";
+		
+		if(Username.equals("admin"))  password.equals("1234");
+		
+		System.out.println("Login Succesfull");
+
+	}  {
+		
+		System.out.println("Login Failed");
+	}
+
+}
