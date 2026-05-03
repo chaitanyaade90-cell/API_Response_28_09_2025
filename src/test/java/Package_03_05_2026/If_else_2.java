c1495ed20c21ca9d255a4edd25885a9a4e6fd499
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-03 22:10:43 +0530

    if else2

---
+++ src/test/java/Package_03_05_2026/If_else_2.java
@@ -0,0 +1,31 @@
+package Package_03_05_2026;
+
+import java.util.Scanner;
+
+public class If_else_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your age");
+		
+		int age = sc.nextInt();
+		
+		if(age>=20) {
+			
+			System.out.println("You can vote ");
+		
+		}else {
+			
+			System.out.println("You can not vote");
+		}
+		
+	
+			
+		}
+
+	}
+
+
