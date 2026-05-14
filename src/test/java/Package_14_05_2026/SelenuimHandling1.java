5247c3d9dc7c3be6b0b79c7f206af7390d84933c
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-14 23:27:54 +0530

    Handling1

---
+++ src/test/java/Package_14_05_2026/SelenuimHandling1.java
@@ -0,0 +1,23 @@
+package Package_14_05_2026;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SelenuimHandling1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.facebook.com");
+		
+		d1.findElement(By.id("username")).sendKeys("Chaitanya");
+
+	}
+
+}
