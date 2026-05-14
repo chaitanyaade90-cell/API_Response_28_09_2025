a74d661781b8350e5a389cc6914d76410a1e18cf
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-14 23:09:43 +0530

    hashmap2

---
+++ src/test/java/Package_14_05_2026/HashMap2.java
@@ -0,0 +1,20 @@
+package Package_14_05_2026;
+
+import java.util.HashMap;
+
+public class HashMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String, String> login = new HashMap<>();
+		
+		login.put("username", "admin");
+		login.put("password", "admin123");
+		
+		System.out.println("Username: " + login.get("username"));
+		System.out.println("Password: " + login.get("password"));
+
+	}
+
+}
