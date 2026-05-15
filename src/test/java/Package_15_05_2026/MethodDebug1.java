6f80bd36bbc993cfe7952847f49b5530121594d8
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-15 23:29:28 +0530

    debug

---
+++ src/test/java/Package_15_05_2026/MethodDebug1.java
@@ -0,0 +1,19 @@
+package Package_15_05_2026;
+
+public class MethodDebug1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		int y = 10;
+		
+		int z = x+y;
+		
+		System.out.println(z);
+		
+		
+
+	}
+
+}
