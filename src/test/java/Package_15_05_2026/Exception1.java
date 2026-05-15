827270366664559773f691675f13c4b6d686debd
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-15 22:59:53 +0530

    exception

---
+++ src/test/java/Package_15_05_2026/Exception1.java
@@ -0,0 +1,23 @@
+package Package_15_05_2026;
+
+public class Exception1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			int a = 10/0;
+			
+		}catch (Exception e) {
+			
+			System.out.println("Exception handled");
+			
+			
+		}
+		
+		System.out.println("programes continues");
+
+	}
+
+}
