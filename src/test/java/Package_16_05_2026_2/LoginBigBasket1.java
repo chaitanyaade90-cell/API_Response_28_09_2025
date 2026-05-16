29819f74026cd53a2c9c9572db766477b1731d43
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-16 23:41:55 +0530

    bigbasketlogin1

---
+++ src/test/java/Package_16_05_2026_2/LoginBigBasket1.java
@@ -0,0 +1,38 @@
+package Package_16_05_2026_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginBigBasket1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		
+		
+		d1.manage().window().maximize();
+		
+		d1.get("https://www.bigbasket.com/");
+		
+		d1.findElement(By.id("username")).sendKeys("chaitanya");
+		
+		d1.findElement(By.id("password")).sendKeys("12345");
+		
+		d1.findElement(By.id("loginButton"));
+		
+		d1.findElement(By.xpath("//*['@id='siteLayout']/header[2]/div[1]/div[2]/div[2]/'button']"));
+		
+		d1.findElement(By.xpath("//*[@id='7897261']"));
+		
+		
+		
+
+	}
+
+}
