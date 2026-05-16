0ca0fc4799a65390b5bd5927edb7cca8510a1dbf
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-05-16 16:18:01 +0530

    mse

---
+++ src/test/java/Package_16_05_2026/Screenshots1.java
@@ -0,0 +1,24 @@
+package Package_16_05_2026;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Screenshots1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.manage().window().maximize();
+		
+		
+		
+		                                                                                                                                                                                                                 
+	}
+
+}
