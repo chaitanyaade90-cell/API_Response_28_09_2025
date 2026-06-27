165616d2039a4894c4df21dc90f77b09839c2fc5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-06-27 13:58:54 +0530

    sele2

---
+++ src/test/java/SeleniumPackage_27_06_2026/Selenium2.java
@@ -0,0 +1,33 @@
+package SeleniumPackage_27_06_2026;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium2 {
+	
+	@Test
+	public void OpenMseb() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.manage().window().maximize();
+		
+		d1.get("https://www.mahadiscom.in/");
+		
+		System.out.println(d1.getTitle());
+		
+		d1.close();
+		
+
+	}
+
+}
