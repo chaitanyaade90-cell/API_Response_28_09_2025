7237e9cbb37b8f94f881394e44a576f29fb21731
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-06-27 12:03:08 +0530

    Static1

---
+++ src/test/java/Static1/Static1.java
@@ -0,0 +1,38 @@
+package Static1;
+
+public class Static1 {
+	
+	static int id;
+	static String name;
+	static int salary;
+	
+	public static void Method1() {
+		
+		System.out.println("My id is " + id);
+	}
+	
+	public static void Method2() {
+		
+		System.out.println("My number is " + name);
+	}
+	
+	public static void Method3() {
+		
+		System.out.println("My salary is " + salary);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Static1.id=22;
+		Static1.name="NFR";
+		Static1.salary=200000;
+		
+		Static1.Method1();
+		Static1.Method2();
+		Static1.Method3();
+
+	}
+
+}
