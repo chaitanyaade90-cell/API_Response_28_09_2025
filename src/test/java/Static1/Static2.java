098007e849b32d21839e96504e16d53859bae450
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2026-06-27 12:25:58 +0530

    static2

---
+++ src/test/java/Static1/Static2.java
@@ -0,0 +1,45 @@
+package Static1;
+
+public class Static2 {
+	
+	static int id;
+	static String name;
+	static int salary;
+	
+	
+	
+	public static void RunTest() {
+		
+		System.out.println("My id is " + id);
+		
+	}
+	
+	public static void SearchResult() {
+		
+		System.out.println("My name is " + name);
+	}
+	
+	public static void Trends() {
+		
+		System.out.println("My salary is " + salary);
+	}
+	
+		
+		
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Static2.id=99;
+		Static2.name="Equal";
+		Static2.salary=90000;
+		
+		Static2.RunTest();
+		Static2.SearchResult();
+		Static2.Trends();
+		
+
+	}
+
+}
